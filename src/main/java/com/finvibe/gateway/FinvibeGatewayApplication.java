8c9b858115ffd9c21801f8317f7c05e393d16ae3
천명현 <cmh1448@naver.com>
2026-04-08 22:39:47 +0900

    First Commit

---
+++ src/main/java/com/finvibe/gateway/FinvibeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.finvibe.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinvibeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinvibeGatewayApplication.class, args);
+	}
+
+}
